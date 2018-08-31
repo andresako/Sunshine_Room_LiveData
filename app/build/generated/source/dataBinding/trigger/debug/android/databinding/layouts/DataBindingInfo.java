a5adb57f8067656388b01eb751bc1afe25cd316c
Andres <andres.martinez@rokigroup.co.uk>
2018-08-31 17:33:40 +0100

    Initial commit

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="655d2f24-439b-4186-89a2-ac8d16a01ff2")
+public class DataBindingInfo {}
